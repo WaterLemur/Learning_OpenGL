a738fd6355b7147e64a01f1eabaa20685f742cec
Water_Lemur(); - /mainframe <lemusdwe@gmail.com>
2026-08-08 19:00:35 -0500

    ***

--- the-cherno-1/video-14/res/src/VertexBufferLayout.hpp
+++ the-cherno-1/video-14/res/src/VertexBufferLayout.hpp
@@ -45,17 +45,17 @@ template<>
 inline void VertexBufferLayout::Push<float>(unsigned int count)
 {
 	m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-	m_Stride += VertexBufferElement::GetSizeOfTipe(GL_FLOAT);
+	m_Stride += count * VertexBufferElement::GetSizeOfTipe(GL_FLOAT);
 }
 template<>
 inline void VertexBufferLayout::Push<int>(unsigned int count)
 {
 	m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-	m_Stride += VertexBufferElement::GetSizeOfTipe(GL_UNSIGNED_INT);
+	m_Stride += count * VertexBufferElement::GetSizeOfTipe(GL_UNSIGNED_INT);
 }
 template<>
 inline void VertexBufferLayout::Push<unsigned char>(unsigned int count)
 {
 	m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-	m_Stride += VertexBufferElement::GetSizeOfTipe(GL_UNSIGNED_BYTE);
+	m_Stride += count * VertexBufferElement::GetSizeOfTipe(GL_UNSIGNED_BYTE);
 }
